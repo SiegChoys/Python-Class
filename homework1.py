b7f0aab171879c29fb414bde2a6795f191e6ac4d
SiegChoys <67082446+SiegChoys@users.noreply.github.com>
2020-12-28 14:22:46 +0900

    Add files via upload

---
+++ homework1.py
@@ -0,0 +1,32 @@
+names = ['이유덕,이재영,권종표,이재영,박민호,강상희,이재영,김지완,최승혁,이성연,박영서,박민호,전경헌,송정환,김재성,이유덕,전경헌']
+i = 0
+
+i = '이유덕,이재영,권종표,이재영,박민호,강상희,이재영,김지완,최승혁,이성연,박영서,박민호,전경헌,송정환,김재성,이유덕,전경헌'.split(',')
+
+print(i)
+
+# 1번 문제
+#print(i.count())
+
+#2번 문제
+print(i.count('이재영'))
+
+#3번 문제
+print(list(set(i)))
+
+#4번 문제
+print
+
+def search (x, y, z):
+    if x or y or z > 90
+        print('둔각 삼각형')
+    elif x or y or z == 90
+        print('직각 삼각형')
+    else
+        print('예각 삼각형')
+
+    return
+
+    x=int(input('제1각을 입력하세요'))
+    y=int(input('제2각을 입력하세요'))
+    z=int(input('제3각을 입력하세요')) 
