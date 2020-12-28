f26b613403e601aba29c28e2d5eaddcaea146c71
SiegChoys <67082446+SiegChoys@users.noreply.github.com>
2020-12-28 19:11:37 +0900

    Add files via upload

--- homework2_triangle.py
+++ homework2_triangle.py
@@ -2,9 +2,9 @@ def search (x, y, z):
     if x+y+z!=180 or x == 0 or y == 0 or z == 0:
         print('삼각형이 아닙니다')
     else:
-        if x or y or z > 90:
+        if x > 90 or y > 90 or z > 90:
             print('둔각 삼각형')
-        elif x or y or z == 90:
+        elif x == 90 or y == 90 or z == 90:
             print('직각 삼각형')
         else:
             print('예각 삼각형')
@@ -13,3 +13,5 @@ def search (x, y, z):
 x=int(input('제1각을 입력하세요'))
 y=int(input('제2각을 입력하세요'))
 z=int(input('제3각을 입력하세요'))
+
+print(search(x,y,z))
