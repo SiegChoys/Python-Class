5304faaf3fe77a74c017da3b8e3057a0c9febb60
SiegChoys <67082446+SiegChoys@users.noreply.github.com>
2020-12-29 20:45:49 +0900

    Add files via upload

--- homework_2_1.py
+++ homework_2_1.py
@@ -4,30 +4,34 @@ k = [41, 43, 45, 47, 49]
 
 numbers = i + j + k
 
-count_2x = 0
-count_not_2x = 0
+count_2 = 0
+count_not_2 = 0
 
 for item in numbers:
     if item % 2 == 0:
-        count_2x +=1
+        count_2 +=1
     else:
-        count_not_2x +=1
+        count_not_2 +=1
 
-print('홀수', count_not_2x, '개,', '짝수', count_2x,'개')
+print('홀수', count_not_2, '개,', '짝수', count_2,'개')
 
 ### --- ###
 
-def func():
-    count_2x = 0
-    count_not_2x = 0
+def func(*i):
+    count_not_2 = 0
+    count_2 = 0
 
-    for item in range:
-        if item % 2 == 0:
-            count_2x +=1
+    numbers = 0
+
+    for i in numbers:
+        if i % 2 == 0:
+            count_2 +=1
+            return(count_2)
         else:
-            count_not_2x +=1
+            count_not_2 +=1
+            return(count_not_2)
 
-print('홀수', count_not_2x, '개,', '짝수', count_2x,'개')
+print('홀수', count_not_2, '개,', '짝수', count_2,'개')
 
 func([3, 4, 5, 6, 7])
 func([12, 16, 22, 24, 29])
--- homework_2_2.py
+++ homework_2_2.py
@@ -1,13 +1,36 @@
 S = [1,3,4,8,13,17,20]
-def distance
-    for i in range
-    i+1-i
+
+difference = 2
+i = 0
+result = int(S[i+1])-int(S[i])
+
+def distance(*i):
+    for i in range(1,len(S)):
+        if result > difference:
+            continue
+        
+        else:
+            difference = result
+        finally: i+1
+    
+        return difference
+
+distance(S)
+
+print(S[i], S[i+1])
+
 # 초기값 설정 - 3-1 = 2
+
 # 위치 기본 설정 = 0
+
 # for 원소의 개수 만큼 range(1,len(S))
+
 # 판단 n ~ n-1 작은지 확인
+
 # 초기값보다 작으면, 초기값을 이 값으로 변경
+
 # 인덱스도 같이 변경
+
 # 아니면 무시
 
 #인쇄 (인덱스-1, 인덱스)
