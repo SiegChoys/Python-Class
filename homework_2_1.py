e14a15b85059fb04902a4addc43f5a48397b0bb1
SiegChoys <67082446+SiegChoys@users.noreply.github.com>
2020-12-30 14:31:06 +0900

    Add files via upload

--- homework_2_1.py
+++ homework_2_1.py
@@ -1,37 +1,33 @@
-i = [3, 4, 5, 6, 7]
-j = [12, 16, 22, 24, 29]
-k = [41, 43, 45, 47, 49]
+#i = [3, 4, 5, 6, 7]
+#j = [12, 16, 22, 24, 29]
+#k = [41, 43, 45, 47, 49]
 
-numbers = i + j + k
+#numbers = i + j + k
 
-count_2 = 0
-count_not_2 = 0
+#count_2 = 0
+#count_not_2 = 0
 
-for item in numbers:
-    if item % 2 == 0:
-        count_2 +=1
-    else:
-        count_not_2 +=1
+#for item in numbers:
+    #if item % 2 == 0:
+        #count_2 +=1
+    #else:
+        #count_not_2 +=1
 
-print('홀수', count_not_2, '개,', '짝수', count_2,'개')
+#print('홀수', count_not_2, '개,', '짝수', count_2,'개')
 
 ### --- ###
 
-def func(*i):
+def func(numlist):
     count_not_2 = 0
     count_2 = 0
 
-    numbers = 0
-
-    for i in numbers:
-        if i % 2 == 0:
+    for i in numlist:
+        if (i % 2 == 0):
             count_2 +=1
-            return(count_2)
         else:
             count_not_2 +=1
-            return(count_not_2)
 
-print('홀수', count_not_2, '개,', '짝수', count_2,'개')
+print('홀수 %s개, 짝수 %s개'%(count_not_2, count_2))
 
 func([3, 4, 5, 6, 7])
 func([12, 16, 22, 24, 29])
---
+++ homework_3_2.py
@@ -0,0 +1,8 @@
+count = 0
+
+if i % 3 == 0
+    sum+=1
+if i % 5 == 0
+    sum+=1
+if i % 15 == 0
+    sum-=1
